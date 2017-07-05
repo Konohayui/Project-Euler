c2f0b35546e5e588fc4dbec24fd0e247a38dd4df
shenmemingzine <rongwingyu@gmail.com>
2017-07-04 17:10:38 -0700

    Update Combinatoric selections.py

--- Combinatoric selections.py
+++ Combinatoric selections.py
@@ -7,9 +7,9 @@ def binom(n, r):
     return fact(n)//fact(r)//fact(n - r)
 
 s = 0
-for m in range(23, 101):
-    for l in range(2, m):
-        if binom(m, l) > 10**6:
+for n in range(23, 101):
+    for r in range(2, m):
+        if binom(n, r) > 10**6:
             s += 1
         
 print(s)
